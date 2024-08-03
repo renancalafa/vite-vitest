e30a72591e199a977916ee12688603850ec42192
Kevin Beltrao de Melo <kbmelo@outlook.com>
2024-08-03 20:45:48 -0300

    adjusting button

--- src/App.tsx
+++ src/App.tsx
@@ -17,21 +17,27 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" style={{ animationDuration: `${seconds}s` }}  />
         </a>
+
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" style={{ animationDuration: `${seconds}s` }}  />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => {setCount((count) => count + 1); setSeconds((seconds) => seconds - (seconds/20))}}>
-          Count is {count} <br></br>
-          Velocity is {(60/seconds).toFixed(1)} RPM
+        <button onClick={() => {setCount((count) => count + 1); setSeconds((seconds) => seconds - (seconds / 20))}}>
+          Count is {count} <br />
+          Velocity is {(60 / seconds).toFixed(1)} RPM
         </button>
-        <Button text={String(count2)} onClick={handleIncrementCount2}/>
+
+        <Button onClick={handleIncrementCount2}>{count2}</Button>
+
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
